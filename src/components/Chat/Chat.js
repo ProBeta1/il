61605f035b87c3197a108711c09e8edae4bbd767
probeta <33081197+ProBeta1@users.noreply.github.com>
2020-08-28 23:10:46 +0530

    front commit

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+import "./Chat.css";
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+import Messages from "../Messages/Messages";
+
+let socket;
+
+const Chat = ({ location }) => {
+
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const [messages, setMessages] = useState([]);
+    const [message, setMessage] = useState('');
+
+    const ENDPOINT = 'https://interview-lelo.herokuapp.com/';
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        socket.emit('join', { name, room }, (error) => {
+            if (error) {
+                do {
+                    alert(error + " Go back, & please take another Username");
+                } while (error);
+            }
+        });
+
+        return () => {
+            socket.emit("disconnect");
+            socket.off();
+        }
+
+    }, [ENDPOINT, location.search]);
+
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message]);
+        })
+    }, [messages])
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+
+        if (message) {
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    }
+
+    console.log(message, messages);
+
+    return (
+        <div className="outerContainer">
+            <div className="container">
+                <InfoBar room={room} />
+                <Messages messages={messages} name={name} />
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+            </div>
+
+        </div>
+    )
+}
+
+export default Chat;
