b63966c459d4c1aba9cc29c440927ac64b71824a
probeta <33081197+ProBeta1@users.noreply.github.com>
2020-09-12 23:52:20 +0530

    major canvas changes made

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -7,7 +7,67 @@ import InfoBar from "../InfoBar/InfoBar";
 import Input from "../Input/Input";
 import Messages from "../Messages/Messages";
 import CanvasDraw from "react-canvas-draw";
+import Slider from '@material-ui/core/Slider';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import { green, red, blue, purple, grey } from '@material-ui/core/colors';
+import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import UndoIcon from '@material-ui/icons/Undo';
 
+const GreenRadio = withStyles({
+    root: {
+        color: green[500],
+        '&$checked': {
+            color: green[600],
+        },
+    },
+    checked: {},
+})((props) => <Radio color="default" {...props} />);
+
+const BlueRadio = withStyles({
+    root: {
+        color: blue[500],
+        '&$checked': {
+            color: blue[600],
+        },
+    },
+    checked: {},
+})((props) => <Radio color="default" {...props} />);
+
+const RedRadio = withStyles({
+    root: {
+        color: red[500],
+        '&$checked': {
+            color: red[600],
+        },
+    },
+    checked: {},
+})((props) => <Radio color="default" {...props} />);
+
+const BlackRadio = withStyles({
+    root: {
+        color: grey[800],
+        '&$checked': {
+            color: grey[900],
+        },
+    },
+    checked: {},
+})((props) => <Radio color="default" {...props} />);
+
+const PurpleRadio = withStyles({
+    root: {
+        color: purple[500],
+        '&$checked': {
+            color: purple[600],
+        },
+    },
+    checked: {},
+})((props) => <Radio color="default" {...props} />);
 
 let socket;
 
@@ -18,6 +78,17 @@ const Chat = ({ location }) => {
     const [messages, setMessages] = useState([]);
     const [message, setMessage] = useState('');
     const [saveableCanvas, setSaveableCanvas] = useState('');
+    const [value, setValue] = useState(3);
+    const [curColor, setCurColor] = useState(purple[600]);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const handleColorChange = (event) => {
+        setCurColor(event.target.value);
+    };
+
 
     const ENDPOINT = 'https://interview-lelo.herokuapp.com/';
 
@@ -64,30 +135,48 @@ const Chat = ({ location }) => {
                 <Messages messages={messages} name={name} />
                 <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
             </div>
+
             <div className="canvas">
-                <button
-                    onClick={() => {
-                        saveableCanvas.undo();
-                    }}
-                    className="undo"
-                >
-                    <h3>Undo</h3>
-                </button>
                 <CanvasDraw
                     canvasWidth="100%"
                     canvasHeight="100%"
                     ref={canvasDraw => (setSaveableCanvas(canvasDraw))}
-                    lazyRadius={10}
-                    brushRadius={5}
+                    lazyRadius={value}
+                    brushRadius={value}
+                    brushColor={curColor}
+                    catenaryColor={curColor}
                 />
-                <button
-                    onClick={() => {
+            </div>
+
+            <div className="slide">
+                <div className="buttons">
+                    <IconButton aria-label="undo" color="primary" onClick={() => {
+                        saveableCanvas.undo();
+                    }}>
+                        <UndoIcon />
+                    </IconButton>
+                    <IconButton aria-label="delete" color="secondary" onClick={() => {
                         saveableCanvas.clear();
-                    }}
-                    className="btn-grad"
-                >
-                    <h3>Clear</h3>
-                </button>
+                    }}>
+                        <DeleteIcon />
+                    </IconButton>
+                </div>
+
+                <div className="picker">
+                    <FormControl component="fieldset">
+                        <RadioGroup aria-label="color" name="color" value={curColor} onChange={handleColorChange}>
+                            <FormControlLabel value={purple[600]} control={<PurpleRadio />} />
+                            <FormControlLabel value="red" control={<RedRadio />} />
+                            <FormControlLabel value={blue[600]} control={<BlueRadio />} />
+                            <FormControlLabel value={green[600]} control={<GreenRadio />} />
+                            <FormControlLabel value="black" control={<BlackRadio />} />
+                        </RadioGroup>
+                    </FormControl>
+                </div>
+                <div className="slider">
+                    <Slider value={value} min={0.5} max={15} orientation="vertical" onChange={handleChange} aria-labelledby="continuous-slider" />
+
+                </div>
             </div>
 
         </div>
